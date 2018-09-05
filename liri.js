d6eef8f0182051547939a6a6c8b0bb2e61e082d8
Brendon Esperancilla <brendon.esp@gmail.com>
2018-09-04 22:45:18 -0400

    import keys.js, build switch cases

--- liri.js
+++ liri.js
@@ -5,31 +5,55 @@ var Spotify = require("node-spotify-api");
 var keys = require("keys.js");
 var fs = require("fs");
 
-var spotify = new Spotify({
-    id: f617959717c84ba8b77658692729c304,
-    secret: e8816bc2fdef4252a29b5ebacdef9c04
-  });
+// var spotify = new Spotify({
+//     id: f617959717c84ba8b77658692729c304,
+//     secret: e8816bc2fdef4252a29b5ebacdef9c04
+// });
+// replaces above code
+var spotify = new Spotify(keys.spotify);
    
-spotify
-    .search(
-        { type: 'track', query: 'All the Small Things' }, function (err, data) {
-            if (err) {
-                return console.log('Error occurred: ' + err);
-            }
-
-            console.log(data);
-        });
 
 var nodeArgs = process.argv;
 
+var command = process.argv[2];
 
+switch(command) {
+    case "concert-this":
+    // stuff
+    break;
 
+    case "spotify-this-song":
+    // stuff
+    break;
+
+    case "movie-this":
+    // stuff
+    break;
+
+    case "do-what-it-says":
+    // stuff;
+    break;
+}
 
 // concert-this
+function concert() {
+
+}
 
 
 
 // spotify-this-song
+function spotifySong() {
+    spotify
+        .search(
+            { type: 'track', query: 'The Sign' }, function (err, data) {
+                if (err) {
+                    return console.log('Error occurred: ' + err);
+                }
+
+                console.log(data);
+            });
+}
 
 
 
