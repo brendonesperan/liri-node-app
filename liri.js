fee38867a9e6672c0c32996a39abb8fb749040a5
Brendon Esperancilla <brendon.esp@gmail.com>
2018-09-08 22:21:34 -0400

    fix spotify error, put switch statements in function, rework functions with arguments

--- liri.js
+++ liri.js
@@ -14,43 +14,53 @@ var spotify = new Spotify(keys.spotify);
    
 
 
-var nodeArgs = process.argv;
-var command = process.argv[2];
+// var nodeArgs = process.argv;
+// var command = process.argv[2];
+var inputName = process.argv.slice(3).join("-");
 
 var textCommand = "";
-var textName = "";
+// var textName = "";
 
-var movieName = "mr+nobody";
-var movieUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+// var movieName = "mr+nobody";
+// var movieUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
-var bandName;
-var bandUrl = "https://rest.bandsintown.com/artists/" + bandName + "/events?app_id=codingbootcamp";
+// var bandName;
+// var bandUrl = "https://rest.bandsintown.com/artists/" + bandName + "/events?app_id=codingbootcamp";
 var timeFormat = "MM/DD/YYYY";
 
-switch(command) {
-    case "concert-this":
-    concertThis();
-    break;
 
-    case "spotify-this-song":
-    spotifyThisSong();
-    break;
 
-    case "movie-this":
-    movieThis();
-    break;
+function commandReader(command) {
+    switch(command) {
+        case "concert-this":
+        concertThis(inputName);
+        break;
 
-    case "do-what-it-says":
-    doWhatItSays();
-    break;
+        case "spotify-this-song":
+        spotifyThisSong(inputName);
+        break;
+
+        case "movie-this":
+        movieThis(inputName);
+        break;
+
+        case "do-what-it-says":
+        doWhatItSays();
+        break;
+
+        default:
+        console.log("Command not recognized.");
+    }
 }
 
+
+
 // concert-this
-function concertThis() {
-    request(bandUrl, function(error, response, body) {
+function concertThis(band) {
+    request("https://rest.bandsintown.com/artists/" + band + "/events?app_id=codingbootcamp", function(error, response, body) {
         if (!error && response.statusCode === 200) {
             var bodyInfo = JSON.parse(body);
-            console.log("For artist: " + bandName);
+            console.log("For artist: " + band);
             console.log("Venue: " + bodyInfo[0].venue.name);
             console.log("Location: " + bodyInfo[0].venue.city);
             console.log("Date: " + moment(bodyInfo[0].datetime, timeFormat));
@@ -61,40 +71,41 @@ function concertThis() {
 
 
 // spotify-this-song
-function spotifyThisSong() {
+function spotifyThisSong(song) {
+    if (!song) {
+        song = "The Sign";
+    }
     spotify
-        .search(
-            { type: 'track', query: 'The Sign' }, function (err, data) {
-                if (err) {
-                    return console.log('Error occurred: ' + err);
-                }
-                console.log(data);
-                spotify.search({ type: 'track', query: song, limit: 1 }, function (err, data) {
-                    if (err) {
-                        return console.log('Error occurred: ' + err);
-                    } else {
-                        console.log(`
+        .search({ type: "track", query: song, limit: 1 }, function (err, data) {
+            if (err) {
+                return console.log("Error occurred: " + err);
+            }
+            else {
+                console.log(`
 Artist(s):      ${data.items[0].album.artists[0].name}
 Song:           ${data.items[0].name}
 Preview:        ${data.items[0].preview_url}
 Album:          ${data.items[0].album.name}
 `);
-                    }
                 
-            });
+            }
+        })
 }
 
 
 
 // movie-this
-function movieThis() {
-    request(movieUrl, function(error, response, body) {
+function movieThis(movie) {
+    if (!movie) {
+        movie = "mr+nobody";
+    }
+    request("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
 
         // If the request is successful
         if (!error && response.statusCode === 200) {
       
           // Parse the body of the site and recover just the imdbRating
-          // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+          // (Note: The syntax below for parsing isn"t obvious. Just spend a few moments dissecting it).
           console.log("Release Year: " + JSON.parse(body).Year);
 
           bodyInfo = JSON.parse(body);
@@ -125,8 +136,9 @@ function doWhatItSays() {
         var randomArr = data.split(",");
         
         textCommand = randomArr[0];
-        textName = randomArr[1];
+        inputName = randomArr[1];
 
+        commandReader(textCommand);
 
         // set global var "command" equal to randomArr[0]
         // run switch statement using new randomArr[0] command
